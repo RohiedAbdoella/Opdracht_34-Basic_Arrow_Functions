1645d14aba408e208dc7a44e0b40113429b85b03
Rohied Abdoella <rohiedabdoella@gmail.com>
2021-07-30 15:01:10 +0200

    Inleveren opdracht: Basic Arrow Functions

---
+++ arrow.js
@@ -0,0 +1,25 @@
+// Deel A
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions();
+
+//Deel B
+const fivePlusSeven = () => 5 + 7
+  
+fivePlusSeven();
+
+//Deel C
+let myFunction = (a, b) => a + b
+
+myFunction();
+
+//Deel D
+let addFive = a => a + 5
+
+addFive();
+
+//Deel E
+let createObject = () => ({greeting: "hoi"})
+
+createObject();
+
